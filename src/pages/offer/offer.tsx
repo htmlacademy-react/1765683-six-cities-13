5cc485f58ce47b264a617590b8b3e00e16204598
r1zyh <kirill.ananiev2012@yandex.ru>
2023-08-08 21:24:19 +0300

    вернет как было

--- src/pages/offer/offer.tsx
+++ src/pages/offer/offer.tsx
@@ -6,7 +6,10 @@ import { TReviews } from '../../types/review';
 import Map from '../../components/map/map';
 import { CITY } from '../../const';
 import PlaceCardList from '../../components/place-card-list/place-card-list';
-import OfferSelected from '../../components/offer-selected/offer-selected';
+import OfferGoods from '../../components/offer-goods/offer-goods';
+import OfferHost from '../../components/offer-host/offer-host';
+import ReviewList from '../../components/review-list/review-list';
+import ReviewForm from '../../components/review-form/review-form';
 
 type OfferProps = {
   offers: TOffers;
@@ -31,7 +34,56 @@ function OfferPage({
       <main className="page__main page__main--offer">
         <section className="offer">
           <OfferImages offers={offers} />
-          <OfferSelected reviews={reviews}/>
+          <div className="offer__container container">
+            <div className="offer__wrapper">
+              <div className="offer__mark">
+                <span>Premium</span>
+              </div>
+              <div className="offer__name-wrapper">
+                <h1 className="offer__name">
+                  Beautiful &amp; luxurious studio at great location
+                </h1>
+                <button className="offer__bookmark-button button" type="button">
+                  <svg className="offer__bookmark-icon" width={31} height={33}>
+                    <use xlinkHref="#icon-bookmark"></use>
+                  </svg>
+                  <span className="visually-hidden">To bookmarks</span>
+                </button>
+              </div>
+              <div className="offer__rating rating">
+                <div className="offer__stars rating__stars">
+                  <span style={{ width: '80%' }}></span>
+                  <span className="visually-hidden">Rating</span>
+                </div>
+                <span className="offer__rating-value rating__value">4.8</span>
+              </div>
+              <ul className="offer__features">
+                <li className="offer__feature offer__feature--entire">
+                  Apartment
+                </li>
+                <li className="offer__feature offer__feature--bedrooms">
+                  3 Bedrooms
+                </li>
+                <li className="offer__feature offer__feature--adults">
+                  Max 4 adults
+                </li>
+              </ul>
+              <div className="offer__price">
+                <b className="offer__price-value">&euro;120</b>
+                <span className="offer__price-text">&nbsp;night</span>
+              </div>
+              <OfferGoods />
+              <OfferHost />
+              <section className="offer__reviews reviews">
+                <h2 className="reviews__title">
+                  Reviews &middot;{' '}
+                  <span className="reviews__amount">{reviews.length}</span>
+                </h2>
+                <ReviewList reviews={reviews} />
+                <ReviewForm />
+              </section>
+            </div>
+          </div>
           <section className="offer__map map">
             <Map
               className={'offer__map'}
