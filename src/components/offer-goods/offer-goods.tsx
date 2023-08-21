5fd84d016e302c5998b165492af9c0d5d611dac2
r1zyh <kirill.ananiev2012@yandex.ru>
2023-08-21 23:02:16 +0300

    Отформатирует код

--- src/components/offer-goods/offer-goods.tsx
+++ src/components/offer-goods/offer-goods.tsx
@@ -8,7 +8,9 @@ function OfferGoods({ goods }: OfferGoodsProps): JSX.Element {
       <h2 className="offer__inside-title">What&apos;s inside</h2>
       <ul className="offer__inside-list">
         {Array.from({ length: goods.length }, (_, i) => (
-          <li className="offer__inside-item" key={i}>{goods[i]}</li>
+          <li className="offer__inside-item" key={i}>
+            {goods[i]}
+          </li>
         ))}
       </ul>
     </div>
